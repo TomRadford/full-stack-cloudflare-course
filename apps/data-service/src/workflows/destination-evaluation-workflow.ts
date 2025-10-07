311c203ea3ee156eb6b9295d549365d40f3dd0c5
Tom Radford <tom@teamgeek.io>
2025-10-07 09:30:38 +0200

    docs: note where I am

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -33,5 +33,7 @@ export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, Desti
 			await this.env.BUCKET.put(r2PathHtml, collectedData.html);
 			await this.env.BUCKET.put(r2PathBodyText, collectedData.bodyText);
 		});
+
+		// YOU ARE AT: 11:50
 	}
 }
