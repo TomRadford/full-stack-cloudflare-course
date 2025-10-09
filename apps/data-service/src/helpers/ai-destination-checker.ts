c94f18b8cd021cbd457db3256e0093f01ebb9690
Tom Radford <tom@theradford.com>
2025-10-09 21:12:07 +0200

    chore: bump `workers-ai-provider`

--- apps/data-service/src/helpers/ai-destination-checker.ts
+++ apps/data-service/src/helpers/ai-destination-checker.ts
@@ -5,7 +5,8 @@ import { z } from 'zod';
 export async function aiDestinationChecker(env: Env, bodyText: string) {
 	const workersAi = createWorkersAI({ binding: env.AI });
 	const result = await generateObject({
-		model: workersAi('@cf/meta/llama-3.1-8b-instruct-fp8'),
+		//@ts-expect-error seems like this model is available but not in types of lib
+		model: workersAi('@cf/meta/llama-3.1-8b-instruct'),
 		prompt:
 			`You will analyze the provided webpage content and determine if it reflects a product that is currently available, not available, or if the status is unclear.
 
